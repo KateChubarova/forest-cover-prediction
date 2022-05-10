c1ba8274f43f527eac7095e46f26da2bc56d5afd
kate.chubarava <k.chubarava@altoros.com>
2022-05-10 11:10:10 +0200

    task 9

--- src/forest_cover_ml/data.py
+++ src/forest_cover_ml/data.py
@@ -3,17 +3,24 @@ from typing import Tuple
 
 import click
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.decomposition import TruncatedSVD
+from sklearn.manifold import TSNE
 
 
 def get_dataset(
-    csv_path_train: Path, csv_path_test
-) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series]:
-    train_data = pd.read_csv(csv_path_train)
-    test_data = pd.read_csv(csv_path_test)
-    click.echo(f"Train dataset shape: {train_data.shape}.")
-    click.echo(f"Test dataset shape: {test_data.shape}.")
-    features_val = train_data["Cover_Type"]
-    features_train = train_data.drop("Cover_Type", axis=1)
-    click.echo(f"Feature train shape: {features_train.shape}")
-    click.echo(f"Feature target shape: {features_val.shape}")
-    return features_train, test_data, features_val
+        csv_path_train: Path, test_split_ratio: float, random_state: int, feature_selection: bool
+) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
+    dataset = pd.read_csv(csv_path_train)
+    click.echo(f"Train dataset shape: {dataset.shape}.")
+
+    features = dataset.drop("Cover_Type", axis=1)
+    target = dataset["Cover_Type"]
+
+    if feature_selection:
+        features = TSNE(n_components=3).fit_transform(features)
+
+    features_train, features_val, target_train, target_val = train_test_split(
+        features, target, test_size=test_split_ratio, random_state=random_state
+    )
+    return features_train, features_val, target_train, target_val
